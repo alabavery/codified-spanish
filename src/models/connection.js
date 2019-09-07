200b5fdda90f1687d0b64efbba6da951ba28e0c7
Al Avery <averyab0@sewanee.edu>
2019-09-07 16:21:33 -0500

    temp heroku db creds

--- src/models/connection.js
+++ src/models/connection.js
@@ -1,7 +1,8 @@
 import Sequelize from 'sequelize';
 
-const sequelize = new Sequelize('codified-spanish', 'alavery', '', {
-    host: 'localhost',
+const sequelize = new Sequelize('dd3am2e2dguhs1', 'juwfhddomnoezp', 'f9db989b90cbefb145aff0dbc50f7ec243de9f4b7eb2756061d56aa762ed5b2c', {
+    host: 'ec2-54-83-9-36.compute-1.amazonaws.com',
+
     dialect: 'postgres',
     define: {
         // camelCase -> snake_case
