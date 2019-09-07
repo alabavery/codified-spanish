4555440c645aad450933560f78e2595fb65b93d0
Al Avery <averyab0@sewanee.edu>
2019-09-07 16:39:25 -0500

    connection

--- src/models/connection.js
+++ src/models/connection.js
@@ -11,7 +11,16 @@ import Sequelize from 'sequelize';
 //         freezeTableName: true,
 //     },
 // });
-const sequelize = new Sequelize('postgres://juwfhddomnoezp:f9db989b90cbefb145aff0dbc50f7ec243de9f4b7eb2756061d56aa762ed5b2c@ec2-54-83-9-36.compute-1.amazonaws.com:5432/dd3am2e2dguhs1');
+const sequelize = new Sequelize('postgres://juwfhddomnoezp:f9db989b90cbefb145aff0dbc50f7ec243de9f4b7eb2756061d56aa762ed5b2c@ec2-54-83-9-36.compute-1.amazonaws.com:5432/dd3am2e2dguhs1', {
+    dialect:  'postgres',
+    protocol: 'postgres',
+    define: {
+        // camelCase -> snake_case
+        underscored: true,
+        // don't add an "s" to table name
+        freezeTableName: true,
+    },
+});
 
 sequelize
     .authenticate()
