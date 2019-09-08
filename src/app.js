bf911fde8cfd66046bc63e49a1a13829945ad20b
Al Avery <averyab0@sewanee.edu>
2019-09-07 20:41:16 -0500

    remove build and deploy npm scripts

--- src/app.js
+++ src/app.js
@@ -7,6 +7,6 @@ app.use('/', graphqlHTTP({
     schema,
     graphiql: true
 }));
-app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT || 4000, () => {
     console.log('Listening...')
 });
