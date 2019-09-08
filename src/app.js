e4f1c25cd2aae1988946678254135bdb7ad33ff5
Al Avery <averyab0@sewanee.edu>
2019-09-07 21:27:45 -0500

    change to test something on heroku

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ app.use('/', graphqlHTTP({
     schema,
     graphiql: true
 }));
-app.listen(process.env.PORT || 4000, () => {
-    console.log('Listening...')
+const port = process.env.PORT || 4000;
+app.listen(port, () => {
+    console.log(`Listening on port ${port}...`)
 });
