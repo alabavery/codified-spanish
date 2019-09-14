ced3d194b967d9cd14f5688ae59be754fcec5188
Al Avery <averyab0@sewanee.edu>
2019-09-14 11:20:03 -0500

    sgfino

--- src/models/connection.js
+++ src/models/connection.js
@@ -21,17 +21,29 @@ pgClient
         console.log(`pg client could not connect to database:`, err);
     });
 
-const sequelize = new Sequelize(dbUrl, {
+// const sequelize = new Sequelize(dbUrl, {
+//     define: {
+//         // camelCase -> snake_case
+//         underscored: true,
+//         // don't add an "s" to table name
+//         freezeTableName: true,
+//     },
+//     ssl: isHeroku,
+//     dialectOptions: {
+//         ssl: isHeroku,
+//     }
+// });
+const sequelize = new Sequelize('postgres://juwfhddomnoezp:f9db989b90cbefb145aff0dbc50f7ec243de9f4b7eb2756061d56aa762ed5b2c@ec2-54-83-9-36.compute-1.amazonaws.com:5432/dd3am2e2dguhs1', {
+    dialect:  'postgres',
+    protocol: 'postgres',
     define: {
         // camelCase -> snake_case
         underscored: true,
         // don't add an "s" to table name
         freezeTableName: true,
     },
-    ssl: isHeroku,
-    dialectOptions: {
-        ssl: isHeroku,
-    }
+    "ssl": true,
+    "dialectOptions": { "ssl": true }
 });
 
 sequelize
