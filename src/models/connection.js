e933b34600af05e51d2416ed3cb5bc451c10c38d
Al Avery <averyab0@sewanee.edu>
2019-09-13 21:52:45 -0500

    use process.env.DATA_BASE_URL for heroku db

--- src/models/connection.js
+++ src/models/connection.js
@@ -10,18 +10,18 @@ const sequelize = new Sequelize('codified-spanish', 'alavery', '', {
         freezeTableName: true,
     },
 });
-// const sequelize = new Sequelize('postgres://juwfhddomnoezp:f9db989b90cbefb145aff0dbc50f7ec243de9f4b7eb2756061d56aa762ed5b2c@ec2-54-83-9-36.compute-1.amazonaws.com:5432/dd3am2e2dguhs1', {
-//     dialect:  'postgres',
-//     protocol: 'postgres',
-//     define: {
-//         // camelCase -> snake_case
-//         underscored: true,
-//         // don't add an "s" to table name
-//         freezeTableName: true,
-//     },
-//     "ssl": true,
-//     "dialectOptions": { "ssl": true }
-// });
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect:  'postgres',
+    protocol: 'postgres',
+    define: {
+        // camelCase -> snake_case
+        underscored: true,
+        // don't add an "s" to table name
+        freezeTableName: true,
+    },
+    "ssl": true,
+    "dialectOptions": { "ssl": true }
+});
 
 sequelize
     .authenticate()
