a45d8f7d580eec1bf2adbdbb1772cb0ecb8c1404
Al Avery <averyab0@sewanee.edu>
2019-09-13 22:55:08 -0500

    rgjeraio

--- src/models/connection.js
+++ src/models/connection.js
@@ -1,22 +1,34 @@
+import pg from 'pg';
 import Sequelize from 'sequelize';
 
-const sequelize = new Sequelize('codified-spanish', 'alavery', '', {
-    host: 'localhost',
-    dialect: 'postgres',
-    define: {
-        // camelCase -> snake_case
-        underscored: true,
-        // don't add an "s" to table name
-        freezeTableName: true,
-    },
+const dbUrl = process.env.DATABASE_URL || `postgres://alavery:''@localhost:5432/codified-spanish`;
+const isHeroku = !!process.env.DATABASE_URL;
+
+const pgClient = new pg.Client({
+    connectionString: dbUrl,
+    ssl: isHeroku,
 });
-const sequelize = new Sequelize(`postgres://juwfhddomnoezp:f9db989b90cbefb145aff0dbc50f7ec243de9f4b7eb2756061d56aa762ed5b2c@ec2-54-83-9-36.compute-1.amazonaws.com:5432/dd3am2e2dguhs1?sslmode=require`, {
+
+pgClient
+    .connect()
+    .then(() => {
+        console.log(`pg client successfully connected to database`);
+    })
+    .catch(err => {
+        console.log(`pg client could not connect to database:`, err);
+    });
+
+const sequelize = new Sequelize(dbUrl, {
     define: {
         // camelCase -> snake_case
         underscored: true,
         // don't add an "s" to table name
         freezeTableName: true,
     },
+    ssl: isHeroku,
+    dialectOptions: {
+        ssl: isHeroku,
+    }
 });
 
 sequelize
